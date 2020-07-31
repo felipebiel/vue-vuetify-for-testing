14f96474a069309b2d3a6ccdecefd826fdad00f4
Felipe Biel <felipe.biel.souza@gmail.com>
2020-07-31 10:58:45 -0300

    new: novo item para exemplos de diagramadores no Painel

---
+++ src/views/pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Painel />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Painel from '@/components/Painel.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Painel
+  }
+}
+</script>
---
+++ src/views/pages/TreeVueChart.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about">
+    <v-container>
+      <v-row class="text-center">
+        <v-col cols="12">
+          <h2
+            class="display-1 font-weight-bold mb-3"
+          >Esse é um Exemplo usando a libary vue-tree-chart</h2>
+        </v-col>
+
+        <v-col cols="12">
+          <VueTreeChart />
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+<script>
+  import VueTreeChart from "@/components/vueTreeChart/VueTreeChart"
+
+  export default {
+    components: {
+      VueTreeChart,
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

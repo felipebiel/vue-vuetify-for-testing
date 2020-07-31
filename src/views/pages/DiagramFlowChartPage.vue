56eaf071e1ba1814f3dd1d592a47534df5326520
Felipe Biel <felipe.biel.souza@gmail.com>
2020-07-31 11:32:25 -0300

    new: Funcionamento Base do FlowChart

--- src/views/pages/DiagramFlowChartPage.vue
+++ src/views/pages/DiagramFlowChartPage.vue
@@ -5,14 +5,72 @@
         <v-col cols="12">
           <h2 class="display-1 font-weight-bold mb-3">Exemplo feito baseado na lib Vue FlowChart</h2>
         </v-col>
-        <v-col cols="12"></v-col>
+        <v-col cols="12">
+          <FlowChart
+            :width="'100%'"
+            :nodes="nodes"
+            :connections="connections"
+            @editnode="handleEditNode"
+            @dblclick="handleDblClick"
+            @editconnection="handleEditConnection"
+            @save="handleChartSave"
+            ref="chart"
+          ></FlowChart>
+        </v-col>
       </v-row>
     </v-container>
   </div>
 </template>
 
 <script>
-export default {};
+import FlowChart from "@/components/DiagramFlowChart/Flowchart";
+
+export default {
+  components: {
+    FlowChart,
+  },
+  data() {
+    return {
+      nodes: [
+        { id: 1, x: 140, y: 270, name: "Start", type: "start" },
+        {
+          id: 2,
+          x: 540,
+          y: 270,
+          name: "End",
+          type: "end",
+          description: "I'm here",
+        },
+      ],
+      connections: [
+        {
+          source: { id: 1, position: "right" },
+          destination: { id: 2, position: "left" },
+          id: 1,
+          type: "pass",
+        },
+      ],
+    };
+  },
+  methods: {
+    handleChartSave(nodes, connections) {
+    },
+    handleEditNode(node) {
+      
+    },
+    handleEditConnection(connection) {},
+    handleDblClick(position) {
+      this.$refs.chart.add({
+        id: +new Date(),
+        x: position.x,
+        y: position.y,
+        name: "New",
+        type: "operation",
+        approvers: [],
+      });
+    },
+  },
+};
 </script>
 
 <style>

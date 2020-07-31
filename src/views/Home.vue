b051c6c3acc5d109688a33853d6f974b00a69dcb
Felipe Biel <felipe.biel.souza@gmail.com>
2020-07-31 10:53:28 -0300

    change: componente de HelloWorld para Painel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <HelloWorld />
+    <Painel />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Painel from '@/components/Painel.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Painel
   }
 }
 </script>

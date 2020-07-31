473bb57fd0348ef05165d7df3ccbd62a29391891
Felipe Biel <felipe.biel.souza@gmail.com>
2020-07-31 11:06:08 -0300

    new: Titulo inical Em DiagramFlowChartPage

--- src/views/pages/DiagramFlowChartPage.vue
+++ src/views/pages/DiagramFlowChartPage.vue
@@ -1,15 +1,19 @@
 <template>
-    <div>
-        diagram flow chart
-    </div>
+  <div class="about">
+    <v-container>
+      <v-row class="text-center">
+        <v-col cols="12">
+          <h2 class="display-1 font-weight-bold mb-3">Exemplo feito baseado na lib Vue FlowChart</h2>
+        </v-col>
+        <v-col cols="12"></v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
 <style>
-
 </style>
